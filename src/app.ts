82c6beb3d48357cdada23aa21529f625ba9668f3
Julfikar-Haidar <julfikar6262@gmail.com>
2024-07-27 18:57:59 +0600

    lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,42 @@
-import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+
+import cookieParser from 'cookie-parser';
+
 const app: Application = express();
-const port = 3000;
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3030', credentials: true }));
+app.use(cookieParser());
 
+//parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: any) => {
-  res.send('Working');
+// app.use('/api/v1/users/', UserRoutes);
+// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+// app.use('/api/v1', routes);
+
+//Testing
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('Testing Error logger')
+// })
+
+//global error handler
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,54 @@
+/* eslint-disable no-console */
+import { Server } from 'http';
 import mongoose from 'mongoose';
-import config from './config/index';
 import app from './app';
+import subscribeToEvents from './app/events';
+import config from './config/index';
+import { errorlogger } from './shared/logger';
+import { RedisClient } from './shared/redis';
+
+process.on('uncaughtException', error => {
+  errorlogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log('Database is connected successfully');
-    console.log(config.port, '9');
-
-    app.listen(config.port, () => {
-      console.log(`app listening on port ${config.port}`);
+    await RedisClient.connect().then(() => {
+      subscribeToEvents();
     });
-  } catch (error) {
-    console.log('Failed to connect database', error);
+
+    await mongoose.connect(config.database_url as string);
+    // logger.info(`🛢   Database is connected successfully`);
+    console.log(`🛢   Database is connected successfully`);
+
+    server = app.listen(config.port, () => {
+      // logger.info(`Application  listening on port ${config.port}`);
+      console.log(`Application  listening on port ${config.port}`);
+    });
+  } catch (err) {
+    errorlogger.error('Failed to connect database', err);
   }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
 }
+
+bootstrap();
+
+// process.on('SIGTERM', () => {
+//   logger.info('SIGTERM is received');
+//   if (server) {
+//     server.close();
+//   }
+// });
