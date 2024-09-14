34f07bfbe893c323b724c4c52748de7215d753d2
Julfikar-Haidar <julfikar6262@gmail.com>
2024-09-14 19:23:42 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,13 @@ import mongoose from 'mongoose';
 import app from './app';
 // import subscribeToEvents from './app/events';
 import config from './config/index';
-// import { errorlogger } from './shared/logger';
-// import { RedisClient } from './shared/redis';
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
     console.log(`🛢   Database is connected successfully`);
 
-    app.listen(config.port, () => {
+    // app.listen(config.port, () => {
       // logger.info(`Application  listening on port ${config.port}`);
       console.log(`Application  listening on port ${config.port}`);
     });
